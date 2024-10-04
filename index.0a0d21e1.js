ad3e920571e6514e1b2a99160e949dfff36fb079
SanGariX <165393583+SanGariX@users.noreply.github.com>
2024-10-04 16:46:37 +0000

    Deploying to gh-pages from @ SanGariX/Again-trainProjectparcel@fc163dbb0187203f234574063185722176daa349 🚀

---
+++ index.0a0d21e1.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".box"),t=document.querySelector(".js-timer"),n=11;setTimeout((function(){e.style.display="flex";var o=setInterval((function(){n-=1,t.textContent=n,n||(clearInterval(o),e.style.display="none")}),1e3)}),1e3)}();
+//# sourceMappingURL=index.0a0d21e1.js.map
---
+++ index.8cda0031.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".box"),t=document.querySelector(".js-timer");let l=11;setTimeout((()=>{e.style.display="flex";const n=setInterval((()=>{l-=1,t.textContent=l,l||(clearInterval(n),e.style.display="none")}),1e3)}),1e3);
+//# sourceMappingURL=index.8cda0031.js.map
