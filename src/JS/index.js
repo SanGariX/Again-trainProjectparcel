0151629c32158d70a01a7da1c62fd6f17e3441ac
SanGariX <165393583+SanGariX@users.noreply.github.com>
2024-10-05 21:13:07 +0300

    0.0.3

--- src/JS/index.js
+++ src/JS/index.js
@@ -29,4 +29,42 @@ setTimeout(()=> {
             box.style.display = "none"
         }
     }, 1000)
-},1000 )
+},0 )
+
+
+//Виклик селекторів
+const day = document.querySelector(".date-day");
+const date = document.querySelector(".date");
+const month = document.querySelector(".date-month");
+const year = document.querySelector(".date-year");
+const digitalClock = document.querySelector(".digital-clock");
+const clockSecondsArrow = document.querySelector(".clock-seconds__arrow");
+const clockMinutesArrow = document.querySelector(".clock-minutes__arrow");
+const clockHoursArrow = document.querySelector(".clock-hours__arrow");
+// Масиви із часом
+const dayArr = ["Неділя", "Понеділок", "Вівторок", "Середа", "Четверг", "П'ятниця", "Субота"]
+const monthArr = ["Січень", "Лютий", "Березень", "Квітень", "Травень", "Червень", "Липень", "Серпень", "Вересень", "Жовтень", "Листопад", "Грудень"]
+let timerSeconds = 0
+// визначення дати
+setInterval(()=>{
+const currentTime = new Date()
+const currentDay = dayArr[currentTime.getDay()]
+const currentMonth = monthArr[currentTime.getMonth()]
+const currentDate = currentTime.getDate()
+const currentYear = currentTime.getFullYear()
+const currentHour = currentTime.getHours()
+const currentMinutes = currentTime.getMinutes()
+const currentSeconds = currentTime.getSeconds()
+const formatTime = `${currentHour.toString().padStart(2, "0")}: ${currentMinutes.toString().padStart(2, "0")}: ${currentSeconds.toString().padStart(2, "0")}`
+const currentSecondTime = 360/60*currentSeconds 
+const currentMinutesTime = 360/60*currentMinutes
+const currentHoursTime = (360/12)*currentHour+currentMinutes+currentSeconds*0.1
+day.textContent = currentDay
+date.textContent = currentDate
+month.textContent = currentMonth
+year.textContent = currentYear
+digitalClock.textContent = `Поточний час: ${formatTime}`  
+clockSecondsArrow.style.transform = `rotate(${currentSecondTime}deg)`
+clockMinutesArrow.style.transform = `rotate(${currentMinutesTime}deg)`
+clockHoursArrow.style.transform = `rotate(${currentHoursTime}deg)`
+}, 1000)
